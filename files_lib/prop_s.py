57d87fdcbc792b8b4d5dab74dab007ead0878e5d
Knoekus <96338562+Knoekus@users.noreply.github.com>
2023-12-08 17:57:57 +0100

    Quick fix Expansions
    Added 'The Abbot' in the Expansions list, and changed reference to said list.

--- files_lib/prop_s.py
+++ files_lib/prop_s.py
@@ -1,5 +1,5 @@
 # Static properties
 font       = 'Microsoft Sans Serif'
 colours    = ['ffffff00', 'ff1414ff', 'ff7d00ff', 'ffff00ff', '14ff14ff', '3232ffff', 'cc32ffff']
-expansions = [r'The River', r'Inns && Cathedrals']
+expansions = ['The Abbot', 'The River', r'Inns && Cathedrals']
 font_sizes = [8, 9, 10, 11, 12, 14, 16, 18, 20, 24, 28, 32, 36, 42, 48, 56, 62, 70, 78, 86, 94]
