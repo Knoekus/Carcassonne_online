4da76fd73584c2b49b536eb69a3a5c9de1fec4fd
Knoekus <96338562+Knoekus@users.noreply.github.com>
2023-12-09 22:44:15 +0100

    Game layout pt1
    First commit for the layout of the Game screen.

--- files_lib/Screens/Game.py
+++ files_lib/Screens/Game.py
@@ -1,20 +1,4 @@
-import sys
-if __name__ != '__main__': # regular call
-    import PyQt5.QtGui     as QtG
-    import PyQt5.QtWidgets as QtW
-    import PyQt5.QtCore    as QtC
-    import PyQt5_Extra     as QtE
-    from firebase_admin import db
-
-    import time
-    import sys
-    import prop_s
-
-    if r"..\Dialogs" not in sys.path:
-        sys.path.append(r"..\Dialogs")
-    from Dialogs.YesNo import YesNoDialog
-    
-else: # direct test call
+if __name__ == '__main__': # direct test call
     import PyQt5.QtGui     as QtG
     import PyQt5.QtWidgets as QtW
     import PyQt5.QtCore    as QtC
@@ -22,9 +6,10 @@ else: # direct test call
     import firebase_admin
     from firebase_admin import credentials, db
     
+    import sys
     if r"..\..\files_lib" not in sys.path:
         sys.path.append(r"..\..\files_lib")
-    import PyQt5_Extra
+    import PyQt5_Extra     as QtE
     import prop_s
     
     if r"..\Dialogs" not in sys.path:
@@ -38,6 +23,18 @@ else: # direct test call
             firebase_admin.initialize_app(cred, {
                 'databaseURL': 'https://clientserver1-default-rtdb.europe-west1.firebasedatabase.app/'
             })
+else: # call from lobby
+    import PyQt5.QtGui     as QtG
+    import PyQt5.QtWidgets as QtW
+    import PyQt5.QtCore    as QtC
+    import PyQt5_Extra     as QtE
+    from firebase_admin import db
+
+    import prop_s    
+    import sys
+    if r"..\Dialogs" not in sys.path:
+        sys.path.append(r"..\Dialogs")
+    from Dialogs.YesNo import YesNoDialog
 
 #%% Game screen
 class GameScreen(QtW.QWidget):
@@ -53,8 +50,6 @@ class GameScreen(QtW.QWidget):
         self.setLayout(self.mainLayout)
     
     def _Game_init(self):
-        self.setWindowTitle('Carcassonne Online')
-        
         # References from lobby
         self.username = self.lobby.username
         self.Refs = self.lobby.Refs
@@ -63,30 +58,79 @@ class GameScreen(QtW.QWidget):
     def _Game_layout(self):
         self.mainLayout = QtW.QGridLayout()
         
-        if __name__ != '__main__':
-            image = QtG.QPixmap(r'.\Images\tile_logo.png')
+        self.mainLayout.addWidget(self._Game_new_tile(),  0, 0, 4, 1)
+        self.mainLayout.addWidget(self._Game_title(),     0, 1, 1, 1)
+        self.mainLayout.addWidget(QtE.QHSeparationLine(), 1, 1, 1, 1)
+        self.mainLayout.addLayout(self._Game_players(),   2, 1, 1, 1)
+        self.mainLayout.addWidget(self._Game_board(),     3, 1, 2, 1)
+        
+        self.mainLayout.setRowStretch(0, 1)
+        self.mainLayout.setRowStretch(1, 0)
+        self.mainLayout.setRowStretch(2, 2)
+        self.mainLayout.setRowStretch(3, 1)
+        self.mainLayout.setRowStretch(self.mainLayout.rowCount()-1, 9)
+        
+    def _Game_new_tile(self):
+        if __name__ == '__main__': # independent call
+            new_tile = QtE.QImage(r'..\Images\tile_logo.png', 250, 250)
+        else: # call from lobby
+            new_tile = QtE.QImage(r'.\Images\tile_logo.png')
+        new_tile.setStyleSheet("padding:10") 
+        return new_tile
+    
+    def _Game_title(self):
+        title = QtW.QLabel("Carcassonne Online")
+        title.setFont(QtG.QFont(prop_s.font,prop_s.font_sizes[5+self.font_size], QtG.QFont.Bold))
+        title.setAlignment(QtC.Qt.AlignCenter)
+        return title
+
+    def _Game_players(self):
+        if __name__ == '__main__':
+            player_list = [f'Player {idx}' for idx in range(1, len(prop_s.colours))]
         else:
-            image = QtG.QPixmap(r'..\Images\tile_logo.png')
-        label = QtW.QLabel()
-        label.setPixmap(image)
+            # Get a new player list
+            players = self.lobby.Refs('connections').get()
+            player_list = [player for player in players if player is not None]
         
-        self.mainLayout.addWidget(label, 0, 0, 1, 1)
+        players = QtW.QGridLayout()
+        for idx, player in enumerate(player_list):
+            name   = QtW.QLabel(f'{player}')
+            name.setFont(QtG.QFont(prop_s.font,prop_s.font_sizes[1+self.font_size], QtG.QFont.Bold))
+            name.setAlignment(QtC.Qt.AlignCenter)
+            points = QtW.QLabel('0')
+            points.setFont(QtG.QFont(prop_s.font,prop_s.font_sizes[1+self.font_size]))
+            points.setAlignment(QtC.Qt.AlignCenter)
+            
+            players.addWidget(name,   1, idx)
+            players.addWidget(points, 2, idx)
         
-        self.leave_button = QtW.QPushButton('Leave')
-        self.mainLayout.addWidget(self.leave_button, 1, 0, 1, 1)
+        # Go from left to right, so fill in the blank spots
+        for padding in range(idx, len(prop_s.colours)-1):
+            players.addWidget(QtW.QLabel(), 1, padding)
+            
+        # players.setRowStretch(0, 1)
+        # players.setRowStretch(1, 1)
+        players.setRowStretch(3, 1)
+            
+        return players
+    
+    def _Game_board(self):
+        board = QtW.QScrollArea()
+        return board
     
     #%% Functionality
     def closeEvent(self, event):
-        title = 'Close program?'
-        text = 'Are you sure you want to close the program?'
-        yesNoDialog = YesNoDialog(self, title, text)
-        result = yesNoDialog.exec_()
-        
-        # Ignore if not accepted, else continue (close)
-        if result != QtW.QDialog.Accepted:
-            event.ignore()
-        else:
-            self.lobby.remove_connection(self.username)
+        if __name__ != '__main__':
+            title = 'Close program?'
+            text = 'Are you sure you want to close the program?'
+            yesNoDialog = YesNoDialog(self, title, text)
+            result = yesNoDialog.exec_()
+            
+            # Ignore if not accepted, else continue (close)
+            if result != QtW.QDialog.Accepted:
+                event.ignore()
+            else:
+                self.lobby.remove_connection(self.username)
 
 #%% Main
 if __name__ == '__main__':
@@ -101,9 +145,6 @@ if __name__ == '__main__':
             self.username = 'Knoekus'
             self.font_size = 5
             self.menu_screen = None
-            
-        def remove_connection(self, username):
-            None
         
         def Refs(self, key, item=None, load='get_set', prefix='lobby'):
             '''load : reference type
