c43634cb12c4abcafa15b9294526cd4132cd7695
Knoekus <96338562+Knoekus@users.noreply.github.com>
2023-12-14 18:13:08 +0100

    Added Expansions framework
    Added framework to add Expansions.
    Implemented the meeples for The Abbot and Inns & Cathedrals expansions in the Inventory.
    Fixed the Inventory layout to allow placement of meeples in first available free cell.

--- files_lib/PyQt6_Extra.py
+++ files_lib/PyQt6_Extra.py
@@ -18,66 +18,69 @@ class ClickableLabel(QtW.QLabel):
         self.clicked.emit()
         
 class QHSeparationLine(QtW.QFrame):
-  '''
-  a horizontal separation line\n
-  '''
-  def __init__(self, height=20, colour:tuple=(0,0,0)):
-    super().__init__()
-    self.setMinimumWidth(1)
-    self.setFixedHeight(height)
-    
-    self.setFrameShape(QtW.QFrame.Shape.HLine)
-    self.setFrameShadow(QtW.QFrame.Shadow.Plain)
-    # self.setSizePolicy(QtW.QSizePolicy.Preferred, QtW.QSizePolicy.Minimum)
-    # policy = QtW.QSizePolicy.setHorizontalStretch(self, 0.8)
-    
-    pal = self.palette()
-    pal.setColor(QtG.QPalette.ColorRole.WindowText, QtG.QColor(colour[0], colour[1], colour[2]))
-    self.setPalette(pal)
-    return
+    '''
+    a horizontal separation line\n
+    '''
+    def __init__(self, height=20, colour:tuple=(0,0,0)):
+        super().__init__()
+        self.setMinimumWidth(1)
+        self.setFixedHeight(height)
+        
+        self.setFrameShape(QtW.QFrame.Shape.HLine)
+        self.setFrameShadow(QtW.QFrame.Shadow.Plain)
+        # self.setSizePolicy(QtW.QSizePolicy.Preferred, QtW.QSizePolicy.Minimum)
+        # policy = QtW.QSizePolicy.setHorizontalStretch(self, 0.8)
+        
+        pal = self.palette()
+        pal.setColor(QtG.QPalette.ColorRole.WindowText, QtG.QColor(colour[0], colour[1], colour[2]))
+        self.setPalette(pal)
+        return
 
 class QVSeparationLine(QtW.QFrame):
-  '''
-  a vertical separation line\n
-  '''
-  def __init__(self, width=20, colour:tuple=(0,0,0)):
-    super().__init__()
-    self.setMinimumHeight(1)
-    self.setFixedWidth(width)
-    
-    self.setFrameShape(QtW.QFrame.Shape.VLine)
-    self.setFrameShadow(QtW.QFrame.Shadow.Plain)
-    # self.setSizePolicy(QtW.QSizePolicy.Minimum, QtW.QSizePolicy.Preferred)
-    
-    pal = self.palette()
-    pal.setColor(QtG.QPalette.ColorRole.WindowText, QtG.QColor(colour[0], colour[1], colour[2]))
-    self.setPalette(pal)
-    return
+    '''
+    a vertical separation line\n
+    '''
+    def __init__(self, width=20, colour:tuple=(0,0,0)):
+        super().__init__()
+        self.setMinimumHeight(1)
+        self.setFixedWidth(width)
+        
+        self.setFrameShape(QtW.QFrame.Shape.VLine)
+        self.setFrameShadow(QtW.QFrame.Shadow.Plain)
+        # self.setSizePolicy(QtW.QSizePolicy.Minimum, QtW.QSizePolicy.Preferred)
+        
+        pal = self.palette()
+        pal.setColor(QtG.QPalette.ColorRole.WindowText, QtG.QColor(colour[0], colour[1], colour[2]))
+        self.setPalette(pal)
+        return
 
 class QImage(QtW.QLabel):
-  '''
-  an image in a label\n
-  '''
-  def __init__(self, file, width=None, height=None):
-      super().__init__()
-      if type(file) == str:
-          self.pixmap = QtG.QPixmap(file)
-          self.setPixmap(self.pixmap)
-      elif type(file) == type(QtG.QPixmap()):
-          self.setPixmap(file)
-          
-      self.setScaledContents(True)
-      if width is not None:
-          self.setFixedWidth(width)
-      if height is not None:
-          self.setFixedHeight(height)
-      return
+    '''
+    an image in a label\n
+    '''
+    def __init__(self, file, width=None, height=None):
+        super().__init__()
+        if type(file) == str:
+            self.pixmap = QtG.QPixmap(file)
+            self.setPixmap(self.pixmap)
+        elif type(file) == type(QtG.QPixmap()):
+            self.setPixmap(file)
+            
+        self.setScaledContents(True)
+        if width is not None:
+            self.setFixedWidth(width)
+        if height is not None:
+            self.setFixedHeight(height)
+      
+        #===== Uncomment for help during development =====#
+        # self.setStyleSheet("background-color:rgba(255,0,0,100)")
+        return
   
 class ClickableImage(QImage):
     clicked = QtC.pyqtSignal()
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.setCursor(QtG.QCursor(QtC.Qt.PointingHandCursor))
+    def __init__(self, file, width=None, height=None, parent=None):
+        super().__init__(file, width, height)
+        self.setCursor(QtG.QCursor(QtC.Qt.CursorShape.PointingHandCursor))
     
     def mousePressEvent(self, event):
         self.clicked.emit()
